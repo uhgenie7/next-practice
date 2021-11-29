f51f51489f8b0d91e8a98f8297c5e0becde319ec
genie <genie@aiskorea.co.kr>
2021-11-29 14:26:58 +0900

    feat: next에서 api 통신하기
    -type: import { NextApiRequest, NextApiResponse } from "next";
    -curl http://localhost:3000/api/todos
    -터미널에 헬로 넥스트가 찍힘

---
+++ next-todo/pages/api/todos.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return res.send("hello Next!");
+};
