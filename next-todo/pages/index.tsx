e774f51466d88735cbd84a20563260f006b376a4
genie <genie@aiskorea.co.kr>
2021-11-29 10:01:36 +0900

    feat: index 에 모으기

---
+++ next-todo/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import TodoList from "./components/TodoList";
+
+const app: NextPage = () => {
+  return <TodoList />;
+};
+
+export default app;
